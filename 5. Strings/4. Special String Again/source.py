60ada886081091311d2c79ee79015e0bca812380
Nikola Vukobrat <nikola.vukobrat@gmail.com>
2020-03-25 21:49:58 +0100

    wip

--- 5. Strings/4. Special String Again/source.py
+++ 5. Strings/4. Special String Again/source.py
@@ -9,7 +9,29 @@ import sys
 
 # Complete the substrCount function below.
 def substrCount(n, s):
-    pass
+    # II attempt
+    # I attempt
+    sub_num = n
+    # Loop in chunks
+    for chunk_size in range(2, n + 1):
+        even_chunk = True if chunk_size % 2 == 0 else False
+        chunk_head = 0
+        while chunk_head + chunk_size <= n:
+            chunk = s[chunk_head:chunk_head + chunk_size]
+            chunk_head += 1
+
+            # Odd/Even case
+            if even_chunk:
+                if len(set(chunk)) == 1:
+                    sub_num += 1
+            else:
+                middle = math.floor(chunk_size / 2)
+                left_half = set(chunk[:middle])
+                right_half = set(chunk[middle + 1:])
+                if (left_half == right_half) and ((len(left_half) == 1) and (len(right_half) == 1)):
+                    sub_num += 1
+
+    return sub_num
 
 
 if __name__ == '__main__':
