b52a3d067f564c793d911cecee43164e14af973a
Nikola Vukobrat <nikola.vukobrat@gmail.com>
2020-04-22 19:41:21 +0200

    Init

---
+++ 6. Greedy Algorithms/3. Greedy Florist/source.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the getMinimumCost function below.
+def getMinimumCost(k, c):
+    pass
+
+
+if __name__ == '__main__':
+    os.environ['OUTPUT_PATH'] = "out.txt"
+
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    c = list(map(int, input().rstrip().split()))
+
+    minimumCost = getMinimumCost(k, c)
+
+    fptr.write(str(minimumCost) + '\n')
+
+    fptr.close()
