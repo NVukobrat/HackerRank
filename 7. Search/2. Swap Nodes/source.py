a1f9682fe3112e2a08ba12c5bb8de2a3d8762532
Nikola Vukobrat <nikola.vukobrat@gmail.com>
2020-05-01 10:30:06 +0200

    Progress

--- 7. Search/2. Swap Nodes/source.py
+++ 7. Search/2. Swap Nodes/source.py
@@ -8,25 +8,40 @@ import sys
 # Complete the swapNodes function below.
 #
 def swapNodes(indexes, queries):
-    print_tree(indexes)
+    # Construct full binary tree
+    i = 0
+    stop_progress = 0
+    stop_goal = len(indexes)
+    while i < len(indexes):
+        new_i = None
+        if indexes[i][0] in {-1, "*"}:
+            new_i = (i * 2) + 1
+        if indexes[i][1] in {-1, "*"}:
+            new_i = (i + 1) * 2
+        if indexes[i][0] not in {"*"} or indexes[i][1] not in {"*"}:
+            stop_progress += 1
+        if new_i is not None:
+            indexes.insert(new_i, ('*', '*'))
+        i += 1
+
+        if stop_progress == stop_goal:
+            break
+
+    # TODO: Error above [case with parent of 10, 11]
+    # Print tree
+    depth = 1
+    added = 0
+    print(1)
+    for r, l in indexes:
+        # print()
+        print(r if r != -1 else "*", l if l != -1 else "*", end=' ')
+        added += 2
+        if added == depth * 2:
+            depth *= 2
+            added = 0
+            print()
 
     return ""
-
-
-def create_tree(indexes):
-    root = Tree()
-    root.data = 1
-
-
-def print_tree(indexes):
-    pass
-
-
-class Tree:
-    def __init__(self):
-        self.data = None
-        self.left = None
-        self.right = None
 
 
 if __name__ == '__main__':
